329e5570c3e7e413085bd2d8600707795adb381a
Steve Downey <sdowney@gmail.com>
2026-08-02 16:12:12 -0400

    Initial copier generation

---
+++ src/smd/backticks/backticks.hpp
@@ -0,0 +1,13 @@
+// name.hpp                                                           -*-C++-*-
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#ifndef INCLUDED_BACKTICKS
+#define INCLUDED_BACKTICKS
+
+#include <string>
+
+// 44cc988c-7353-43aa-a7d3-8840f92371a6
+namespace backticks {
+std::string_view backticks();
+}
+// 44cc988c-7353-43aa-a7d3-8840f92371a6 end
+#endif // INCLUDED_BACKTICKS
